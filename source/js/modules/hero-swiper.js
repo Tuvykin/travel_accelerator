0c974974e220125f0df1597fa34d82365b3fae47
nikolay tuvykin <tuvykin93@yandex.ru>
2023-12-07 00:28:22 +0300

    добавляет хиро

--- source/js/modules/hero-swiper.js
+++ source/js/modules/hero-swiper.js
@@ -2,10 +2,8 @@ export const initHeroSlider = () => {
   const swiperWrapper = document.querySelector('[data-hero-swiper]');
 
   return new window.Swiper(swiperWrapper, {
-    loop: true,
-
     pagination: {
-      el: '[data-swiper-pagination]',
+      el: '[data-swiper-hero-pagination]',
     },
   });
 };
