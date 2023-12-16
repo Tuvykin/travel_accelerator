9602ad281f19750310b3be141719a19a1530d2b5
nikolay tuvykin <tuvykin93@yandex.ru>
2023-12-17 00:19:30 +0300

    добавляет работу с тестом

--- source/js/modules/tours-swiper.js
+++ source/js/modules/tours-swiper.js
@@ -9,16 +9,19 @@ export const initToursSlider = () => {
       // when window width is >= 320px
       320: {
         slidesPerView: 1,
+        initialSlide: 3,
       },
       // when window width is >= 768px
       768: {
         slidesPerView: 2,
         spaceBetween: 18,
+        initialSlide: 3,
       },
       // when window width is >= 1200px
       1240: {
         slidesPerView: 3,
         spaceBetween: 30,
+        initialSlide: 4,
       },
     },
     navigation: {
--- source/js/modules/training-swiper.js
+++ source/js/modules/training-swiper.js
@@ -9,16 +9,19 @@ export const initTrainingSlider = () => {
       // when window width is >= 320px
       320: {
         slidesPerView: 1,
+        initialSlide: 2,
       },
       // when window width is >= 768px
       768: {
         slidesPerView: 3,
         spaceBetween: 20,
+        initialSlide: 4,
       },
       // when window width is >= 1200px
       1240: {
         slidesPerView: 4,
         spaceBetween: 20,
+        initialSlide: 4,
       },
     },
     navigation: {
