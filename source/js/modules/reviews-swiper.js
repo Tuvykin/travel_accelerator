3dd38c20b62d0adb04da93f2a69a205d6ca50cf3
nikolay tuvykin <tuvykin93@yandex.ru>
2023-12-19 14:32:35 +0300

    добавляет правильные отзывы

--- source/js/modules/reviews-swiper.js
+++ source/js/modules/reviews-swiper.js
@@ -4,24 +4,24 @@ export const initReviewsSlider = () => {
   const buttonPrev = document.querySelector('[data-reviews-swiper-btn-prev]');
 
   return new window.Swiper(swiperWrapper, {
-    loop: false,
+    loop: true,
     breakpoints: {
       // when window width is >= 320px
       320: {
         slidesPerView: 1,
-        initialSlide: 1,
+        initialSlide: 0,
       },
       // when window width is >= 768px
       768: {
         slidesPerView: 'auto',
         spaceBetween: 30,
-        initialSlide: 1.5,
+        initialSlide: 0.5,
       },
       // when window width is >= 1200px
       1200: {
-        slidesPerView: 1.6,
+        slidesPerView: 1.605,
         spaceBetween: 120,
-        initialSlide: 1,
+        initialSlide: 0,
       },
     },
     navigation: {
